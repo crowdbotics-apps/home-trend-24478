a872645baabe13227f0dffd2b03678c618a4c9ff
Crowdbotics <team@crowdbotics.com>
2021-02-16 20:43:19 +0000

    remove package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -3,7 +3,6 @@ export const installed_blueprints = [
 { name: 'UserProfile75206323', human_name: 'User Profile 75', access_route: 'UserProfile75206323'},
 { name: 'UserProfile204398', human_name: 'User Profile', access_route: 'UserProfile204398'},
 { name: 'Tutorial204397', human_name: 'Tutorial', access_route: 'Tutorial204397', icon: 'question-circle'},
-{ name: 'NotificationList204369', human_name: 'Notification List', access_route: 'NotificationList204369'},
 { name: 'Settings204368', human_name: 'Settings', access_route: 'Settings204368'},
 { name: 'Settings204360', human_name: 'Settings', access_route: 'Settings204360'},
 { name: 'UserProfile204358', human_name: 'User Profile', access_route: 'UserProfile204358'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from './sideMenu';
 import UserProfile75206323Navigator from '../features/UserProfile75206323/navigator';
 import UserProfile204398Navigator from '../features/UserProfile204398/navigator';
 import Tutorial204397Navigator from '../features/Tutorial204397/navigator';
-import NotificationList204369Navigator from '../features/NotificationList204369/navigator';
 import Settings204368Navigator from '../features/Settings204368/navigator';
 import Settings204360Navigator from '../features/Settings204360/navigator';
 import UserProfile204358Navigator from '../features/UserProfile204358/navigator';
@@ -23,7 +22,6 @@ const AppNavigator = {
 UserProfile75206323: { screen: UserProfile75206323Navigator },
 UserProfile204398: { screen: UserProfile204398Navigator },
 Tutorial204397: { screen: Tutorial204397Navigator },
-NotificationList204369: { screen: NotificationList204369Navigator },
 Settings204368: { screen: Settings204368Navigator },
 Settings204360: { screen: Settings204360Navigator },
 UserProfile204358: { screen: UserProfile204358Navigator },
