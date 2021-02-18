11c00f1c277a48c6be7bb106d97a91aee6a8a70a
Crowdbotics <team@crowdbotics.com>
2021-02-18 10:52:41 +0000

    remove package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -42,7 +42,6 @@ export const installed_blueprints = [
 { name: 'UserProfile204398', human_name: 'User Profile', access_route: 'UserProfile204398'},
 { name: 'Tutorial204397', human_name: 'Tutorial', access_route: 'Tutorial204397', icon: 'question-circle'},
 { name: 'Settings204368', human_name: 'Settings', access_route: 'Settings204368'},
-{ name: 'Settings204360', human_name: 'Settings', access_route: 'Settings204360'},
 { name: 'UserProfile204358', human_name: 'User Profile', access_route: 'UserProfile204358'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -46,7 +46,6 @@ import BlankScreen41206502Navigator from '../features/BlankScreen41206502/naviga
 import UserProfile75206323Navigator from '../features/UserProfile75206323/navigator';
 import UserProfile204398Navigator from '../features/UserProfile204398/navigator';
 import Settings204368Navigator from '../features/Settings204368/navigator';
-import Settings204360Navigator from '../features/Settings204360/navigator';
 import UserProfile204358Navigator from '../features/UserProfile204358/navigator';
 
 /**
@@ -97,7 +96,6 @@ BlankScreen41206502: { screen: BlankScreen41206502Navigator },
 UserProfile75206323: { screen: UserProfile75206323Navigator },
 UserProfile204398: { screen: UserProfile204398Navigator },
 Settings204368: { screen: Settings204368Navigator },
-Settings204360: { screen: Settings204360Navigator },
 UserProfile204358: { screen: UserProfile204358Navigator },
 
     /** new navigators can be added here */
